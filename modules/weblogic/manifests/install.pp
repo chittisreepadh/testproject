771c96795ea60220d4594309be6c8a66677dbd3a
bandaru <bandaru@pemaster.eidikointernal.com>
2018-01-02 16:37:22 -0500

    test24

--- modules/weblogic/manifests/install.pp
+++ modules/weblogic/manifests/install.pp
@@ -1,6 +1,4 @@
 class weblogic::install {
-exec {"permissions":
-command => "/usr/sbin/usermod -G vboxsf oracle", }
 exec { "installation" :
 command => "/usr/local/java/bin/java -jar /media/sf_bandaru/weblogic/fmw_12.2.1.2.0_wls.jar -silent -responseFile /media/sf_bandaru/wlsinstallation.rsp -invPtrLoc /u05/oraInst.loc",
 user    => oracle,
